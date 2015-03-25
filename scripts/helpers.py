1db26b1887b9aa32fa0b9bab55272776ad4779a5
Will Engler <engler.will@gmail.com>
2015-03-25 16:10:39 -0400

    Begin preppping data for logistic regression

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,5 +1,6 @@
 import csv
 
+
 def bin_from_csv(csv_name, series_to_bin):
     with open(csv_name, 'rb') as bin_file:
         unbinned_to_binned = {}
@@ -8,3 +9,11 @@ def bin_from_csv(csv_name, series_to_bin):
             unbinned_to_binned[line[0]] = line[1]
 
     return series_to_bin.map(lambda unbinned: unbinned_to_binned[unbinned])
+
+
+def make_col_categorical(data_frame, col_name):
+    data_frame[col_name] = data_frame[col_name].astype('category')
+
+
+def transform_from_csv(data_frame, col_name, csv_name):
+    data_frame[col_name] = bin_from_csv(csv_name, data_frame[col_name])
