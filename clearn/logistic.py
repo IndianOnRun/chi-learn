62ec802c1c6b6f6edf393394fe9a63e9be7eba05
Will Engler <engler.will@gmail.com>
2015-03-27 15:00:03 -0400

    Sketch out how final feature vector will be constructed

--- clearn/logistic.py
+++ clearn/logistic.py
@@ -12,10 +12,8 @@ def get_target_vectors(data_frame):
 
     """
 
-    crimes = hlp.drop_all_columns_but(data_frame, ['Date', 'Primary Type', 'Community Area'])
-    crimes = hlp.convert_comm_area_nums_to_names(crimes)
+    crimes = make_clean_timestamps(data_frame)
     crimes = hlp.drop_nonviolent_crimes(crimes)
-    crimes = hlp.reindex_by_date(crimes)
 
     # Add a new column that we'll use to sum crimes per day
     crimes['Violent Crime'] = 1
@@ -31,3 +29,27 @@ def get_target_vectors(data_frame):
         targets[name] = area_crimes['Violent Crime Committed'].values
 
     return targets
+
+
+def get_feature_vectors(data_frame):
+    crimes = make_clean_timestamps(data_frame)
+    pan_chicago_series = get_summary(crimes)
+    community_area_features = {}
+    grouped_by_community = crimes.groupby('Community Area')
+    for name, frame in grouped_by_community:
+        area_series = get_summary(frame)
+        # Concatenate the chicago stats to the community area stats
+        community_area_features[name] = area_series.values
+
+def get_summary(crimes):
+    """  """
+    pass
+
+
+def make_clean_timestamps(data_frame):
+    crimes = hlp.drop_all_columns_but(data_frame, ['Date', 'Primary Type', 'Community Area', 'Arrest', 'Domestic'])
+    crimes = hlp.convert_comm_area_nums_to_names(crimes)
+    crimes = hlp.transform_from_csv(crimes,'Primary Type','../config/crime_bins.csv')
+    crimes = hlp.reindex_by_date(crimes)
+    crimes = hlp.make_col_categorical(crimes, 'Primary Type')
+    return hlp.make_col_categorical(crimes, 'Community Area')
