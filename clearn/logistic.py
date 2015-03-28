685d9851cd6c53bae5fcdb4e582b2d972311b856
Will Engler <engler.will@gmail.com>
2015-03-28 16:11:15 -0400

    Finish untested training set generator

--- clearn/logistic.py
+++ clearn/logistic.py
@@ -2,48 +2,7 @@ import pandas as pd
 import csv
 
 
-class TrainingExample:
-
-    def __init__(self):
-        pass
-
-
-class DailyCrimesFactory:
-    """
-        Given a pandas dataframe of "clean" timestamps,
-        Create a dataframe indexed by day with
-        extra rolling window features,
-    """
-
-    def __init__(self, crimes, include_time_features=False):
-        self.crimes = crimes
-        self.extract_severity_counts()
-        self.resample()
-        if include_time_features:
-            self.extract_time_features()
-        self.extract_windows()
-
-    def extract_time_features(self):
-        self.crimes['Year'] = self.crimes.index.map(lambda stamp: stamp.year)
-        self.crimes['Month'] = self.crimes.index.map(lambda stamp: stamp.month)
-        self.crimes['Weekday'] = self.crimes.index.map(lambda stamp: stamp.weekday())
-        self.crimes = make_cols_categorical(self.crimes, ['Year', 'Month', 'Weekday'])
-
-    def extract_severity_counts(self):
-        for severity in ['Violent', 'Severe', 'Minor', 'Petty']:
-            self.crimes[severity + ' Crimes'] = [int(category == severity) for category in self.crimes['Primary Type']]
-
-    def resample(self):
-        self.crimes.resample('D', how='sum')
-
-    def extract_windows(self):
-        for severity in ['Violent', 'Severe', 'Minor', 'Petty']:
-            self.crimes[severity + ' Crimes in Last Week'] = \
-                pd.rolling_sum(self.crimes[severity + ' Crimes'], 7)
-            self.crimes[severity + ' Crimes in Last Month'] = \
-                pd.rolling_sum(self.crimes[severity + ' Crimes'], 30)
-
-
+# TODO: Drop examples with missing data because of moving window function
 class TrainingSetFactory:
     """Given a csv of Chicago crime data,
     Creates a training set with feature and target vectors"""
@@ -51,10 +10,8 @@ class TrainingSetFactory:
     def __init__(self, data_csv_path):
         self.data_frame = pd.read_csv(data_csv_path)
         self.make_clean_timestamps()
-        area_to_daily_crimes = self.get_daily_crimes_by_area()
-        pan_chicago_daily_crimes = DailyCrimesFactory(self.crimes).crimes
-        concatenated_crimes_by_area = self.concat_city_with_areas(pan_chicago_daily_crimes, area_to_daily_crimes)
-        self.feature_vectors_by_area = self.get_feature_vectors(concatenated_crimes_by_area)
+        self.target_vectors = TargetVectorsFactory(self.data_frame)
+        self.feature_vectors_by_area = self.get_feature_vectors()
 
     def make_clean_timestamps(self):
         self.drop_all_columns_but(['Date', 'Primary Type', 'Community Area', 'Arrest', 'Domestic'])
@@ -62,6 +19,19 @@ class TrainingSetFactory:
         self.transform_from_csv('Primary Type', '../config/crime_bins.csv')
         self.reindex_by_date()
         self.data_frame = make_cols_categorical(self.data_frame, ['Primary Type', 'Community Area'])
+
+    def get_feature_vectors(self):
+        area_to_daily_crimes = self.get_daily_crimes_by_area()
+        pan_chicago_daily_crimes = DailyCrimesFactory(self.crimes).crimes
+        concatenated_crimes_by_area = self.concat_city_with_areas(pan_chicago_daily_crimes, area_to_daily_crimes)
+        return self.extract_features_from_data_frame(concatenated_crimes_by_area)
+
+    @staticmethod
+    def extract_features_from_data_frame(crimes_by_area):
+        feature_vectors_by_area = {}
+        for area in crimes_by_area:
+            feature_vectors_by_area[area] = crimes_by_area[area].values
+        return feature_vectors_by_area
 
     def drop_all_columns_but(self, relevant_columns):
         self.data_frame = self.data_frame.reindex(columns=relevant_columns)
@@ -105,50 +75,64 @@ class TrainingSetFactory:
             concatenated_crimes_by_area[area] = area_crimes
         return concatenated_crimes_by_area
 
-    @staticmethod
-    def get_feature_vectors(crimes_by_area):
-        feature_vectors_by_area = {}
-        for area in crimes_by_area:
-            feature_vectors_by_area[area] = crimes_by_area[area].values
-        return feature_vectors_by_area
 
+class TargetVectorsFactory:
+    def __init__(self, crimes):
+        self.crimes = crimes
+        self.drop_nonviolent_crimes()
+        # Add a new column that we'll use to sum crimes per day
+        self.crimes['Violent Crime'] = 1
+        grouped = self.crimes.groupby('Community Area')
+        self.targets = {}
+        for name, frame in grouped:
+            # Make one row per day
+            area_crimes = frame.resample('D', how='sum')
+            # Create new column that is true iff at least one violent crime was committed that day
+            area_crimes['Violent Crime Committed'] = area_crimes['Violent Crime'].map(
+                lambda num_crimes: num_crimes > 0)
+            # Store the raw True/False values in the target vector
+            self.targets[name] = area_crimes['Violent Crime Committed'].values
+
+    def drop_nonviolent_crimes(self):
+        self.crimes = self.crimes[self.crimes['Primary Type'] == 'Violent']
+
+
+class DailyCrimesFactory:
+    """
+        Given a pandas dataframe of "clean" timestamps,
+        Create a dataframe indexed by day with
+        extra rolling window features,
+    """
+
+    def __init__(self, crimes, include_time_features=False):
+        self.crimes = crimes
+        self.extract_severity_counts()
+        self.resample()
+        if include_time_features:
+            self.extract_time_features()
+        self.extract_windows()
+
+    def extract_time_features(self):
+        self.crimes['Year'] = self.crimes.index.map(lambda stamp: stamp.year)
+        self.crimes['Month'] = self.crimes.index.map(lambda stamp: stamp.month)
+        self.crimes['Weekday'] = self.crimes.index.map(lambda stamp: stamp.weekday())
+        self.crimes = make_cols_categorical(self.crimes, ['Year', 'Month', 'Weekday'])
+
+    def extract_severity_counts(self):
+        for severity in ['Violent', 'Severe', 'Minor', 'Petty']:
+            self.crimes[severity + ' Crimes'] = [int(category == severity) for category in self.crimes['Primary Type']]
+
+    def resample(self):
+        self.crimes.resample('D', how='sum')
+
+    def extract_windows(self):
+        for severity in ['Violent', 'Severe', 'Minor', 'Petty']:
+            self.crimes[severity + ' Crimes in Last Week'] = \
+                pd.rolling_sum(self.crimes[severity + ' Crimes'], 7)
+            self.crimes[severity + ' Crimes in Last Month'] = \
+                pd.rolling_sum(self.crimes[severity + ' Crimes'], 30)
 
 def make_cols_categorical(data_frame, col_names):
         for name in col_names:
             data_frame[name] = data_frame[name].astype('category')
         return data_frame
-
-
-def get_target_vectors(data_frame):
-    """
-    Returns dictionary mapping community area names
-    to boolean-valued numpy vectors. Each value represents whether
-    a violent crime was committed on a day. They are listed in ascending order
-    of time
-    e.g. Edgewater -> [True, False, ...]
-
-    """
-
-    crimes = make_clean_timestamps(data_frame)
-    crimes = hlp.drop_nonviolent_crimes(crimes)
-
-    # Add a new column that we'll use to sum crimes per day
-    crimes['Violent Crime'] = 1
-    grouped = crimes.groupby('Community Area')
-    targets = {}
-    for name, frame in grouped:
-        # Make one row per day
-        area_crimes = frame.resample('D', how='sum')
-        # Create new column that is true iff at least one violent crime was committed that day
-        area_crimes['Violent Crime Committed'] = area_crimes['Violent Crime'].map(
-            lambda num_crimes: num_crimes > 0)
-        # Store the raw True/False values in the target vector
-        targets[name] = area_crimes['Violent Crime Committed'].values
-
-    return targets
-
-
-def drop_nonviolent_crimes(data_frame):
-    data_frame = transform_from_csv(data_frame, 'Primary Type', '../config/crime_bins.csv')
-    data_frame = data_frame[data_frame['Primary Type'] == 'Violent']
-    return data_frame
