3e0a78ee5766cf3979f7191c03611b05389ca6a8
Will Engler <engler.will@gmail.com>
2015-04-11 15:04:37 -0400

    Old Markov tests pass

--- clearn/predict_tests.py
+++ clearn/predict_tests.py
@@ -3,32 +3,36 @@ import pandas as pd
 from clearn import predict
 
 
-class MarkovTests(unittest.TestCase):
+class SequentialTests(unittest.TestCase):
     def setUp(self):
         # Create index of 32 dates from arbitrary start point
         date_sequence = pd.date_range('1/1/2011', periods=32, freq='D')
         # Try to predict the last date in the sequence
         self.date_to_predict = date_sequence[-1]
-        self.df = pd.DataFrame(index=date_sequence)
+        self.time_series = pd.DataFrame(index=date_sequence)
     
     def test_series_with_all_violent_days(self):
         # 1 means a violent crime was committed
-        self.df['Violent Crime Committed?'] = [1]*32
-        prediction = predict.sequential(self.df, self.date_to_predict)
+        self.time_series['Violent Crime Committed?'] = [1]*32
+        predictor = predict.SequentialPredictor(self.time_series)
+        prediction = predictor.predict(self.date_to_predict)
         self.assertEqual(prediction, 1)
 
     def test_series_with_no_violent_days(self):
         # 0 Means no violent crime was committed
-        self.df['Violent Crime Committed?'] = [0]*32
-        prediction = predict.sequential(self.df, self.date_to_predict)
+        self.time_series['Violent Crime Committed?'] = [0]*32
+        predictor = predict.SequentialPredictor(self.time_series)
+        prediction = predictor.predict(self.date_to_predict)
         self.assertEqual(prediction, 0)
 
     def test_series_with_one_violent_day(self):
-        self.df['Violent Crime Committed?'] = [0, 0, 1] + [0]*29
-        prediction = predict.sequential(self.df, self.date_to_predict)
+        self.time_series['Violent Crime Committed?'] = [0, 0, 1] + [0]*29
+        predictor = predict.SequentialPredictor(self.time_series)
+        prediction = predictor.predict(self.date_to_predict)
         self.assertEqual(prediction, 0)
 
     def test_series_with_one_nonviolent_day(self):
-        self.df['Violent Crime Committed?'] = [1, 1, 0] + [1]*29
-        prediction = predict.sequential(self.df, self.date_to_predict)
+        self.time_series['Violent Crime Committed?'] = [1, 1, 0] + [1]*29
+        predictor = predict.SequentialPredictor(self.time_series)
+        prediction = predictor.predict(self.date_to_predict)
         self.assertEqual(prediction, 1)
